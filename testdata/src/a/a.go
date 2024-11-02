77932dfcc32e569fcd534ff5f6ef562dfc3a3f98
市川恭佑 <github@mail.ebi-yade.com>
2024-11-02 22:22:10 +0900

    Create simple testdata

--- testdata/src/a/a.go
+++ testdata/src/a/a.go
@@ -1,7 +1,48 @@
 package a
 
-func f() {
-	// The pattern can be written in regular expression.
-	var gopher int // want "pattern"
-	print(gopher)  // want "identifier is gopher"
+import (
+	"context"
+	"time"
+
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/attribute"
+)
+
+var tracer = otel.Tracer("a")
+
+func endless(ctx context.Context) error {
+	_, span := tracer.Start(ctx, "a") // want "missing span.End() in the function scope"
+
+	sth, err := fetchSomething(ctx)
+	if err != nil {
+		return err
+	}
+
+	span.SetAttributes(attribute.String("sth.id", sth.ID))
+	return nil
+}
+
+func endful(ctx context.Context) error {
+	_, span := tracer.Start(ctx, "a")
+	defer span.End()
+
+	sth, err := fetchSomething(ctx)
+	if err != nil {
+		return err
+	}
+
+	span.SetAttributes(attribute.String("sth.id", sth.ID))
+	return nil
+}
+
+type something struct {
+	ID string
+}
+
+func fetchSomething(ctx context.Context) (something, error) {
+	time.Sleep(100 * time.Millisecond)
+
+	return something{
+		ID: "hoge",
+	}, nil
 }
