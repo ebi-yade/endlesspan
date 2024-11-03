786591d6e6067646987d7d173196685520343806
市川恭佑 <github@mail.ebi-yade.com>
2024-11-03 20:06:35 +0900

    Update tests

--- testdata/src/a/a.go
+++ testdata/src/a/a.go
@@ -6,12 +6,13 @@ import (
 
 	"go.opentelemetry.io/otel"
 	"go.opentelemetry.io/otel/attribute"
+	"go.opentelemetry.io/otel/trace"
 )
 
 var tracer = otel.Tracer("a")
 
 func endless(ctx context.Context) error {
-	_, span := tracer.Start(ctx, "a") // want "missing span.End() in the function scope"
+	_, span := tracer.Start(ctx, "a") // want "missing span.End() call in the scope"
 
 	sth, err := fetchSomething(ctx)
 	if err != nil {
@@ -20,6 +21,11 @@ func endless(ctx context.Context) error {
 
 	span.SetAttributes(attribute.String("sth.id", sth.ID))
 	return nil
+}
+
+func returningSpan(ctx context.Context) (trace.Span, error) {
+	_, span := tracer.Start(ctx, "a")
+	return span, nil
 }
 
 func endful(ctx context.Context) error {
@@ -33,6 +39,37 @@ func endful(ctx context.Context) error {
 
 	span.SetAttributes(attribute.String("sth.id", sth.ID))
 	return nil
+}
+
+func nonDefer(ctx context.Context) error {
+	_, span := tracer.Start(ctx, "a")
+
+	sth, err := fetchSomething(ctx)
+	if err != nil {
+		return err
+	}
+
+	span.SetAttributes(attribute.String("sth.id", sth.ID))
+	span.End() // want "you may want to defer the span.End() call"
+	return nil
+}
+
+var anonymous = func(ctx context.Context) error {
+	_, span := tracer.Start(ctx, "a") // want "missing span.End() call in the scope"
+
+	sth, err := fetchSomething(ctx)
+	if err != nil {
+		return err
+	}
+
+	span.SetAttributes(attribute.String("sth.id", sth.ID))
+	return nil
+}
+
+var anonymousReturningSpan = func(ctx context.Context) (trace.Span, error) {
+	_, span := tracer.Start(ctx, "a")
+
+	return span, nil
 }
 
 type something struct {
